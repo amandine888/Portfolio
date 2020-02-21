569a3beb03837223805da23515f1d7565c6e1e9e
Amandine <amandine2.divay@gmail.com>
2020-02-21 10:42:24 +0100

    integration component Project

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 import {withRouter} from 'react-router-dom'; 
 import NavBarre from './NavBarre'; 
-import Title from './Title'; 
-import './../mystyle.css';
+import Title from './Title';
+import './../Project.css';
 
 class Project extends React.Component {
     render (){
@@ -10,19 +10,25 @@ class Project extends React.Component {
             <div>
                 <NavBarre/>
                 <Title/>
-                <div>
+                <div className="sentenceContainer">
                     <div className='sentence'>
                         <h3>DEVELOPPEMENT WEB - <span className="myName">NOMUSE -</span> WEBDESIGN - CONSULTING</h3>
                     </div>
                     <div className='sentence'>
-                        <h3>DEVELOPPEMENT WEB - <span className="myName">AKUAPÉLI -</span> WEBDESIGN - CONSULTING</h3>
+                        <div className='position'>
+                            <h3>DEVELOPPEMENT WEB - <span className="myName">AKUAPÉLI -</span> WEBDESIGN - CONSULTING</h3>
+                        </div>
                     </div>
                     <div className='sentence'>
                         <h3>DEVELOPPEMENT WEB - <span className="myName">PUISSANCE 4 -</span> WEBDESIGN - CONSULTING</h3>
                     </div>
+                    <div className='sentence'>
+                        <div className='position'>
+                            <h3>DEVELOPPEMENT WEB - <span className="myName">MISORA -</span> WEBDESIGN - CONSULTING</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
         )
     }
 }
