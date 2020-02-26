1d4f4dbc513a1ad11cc39df600dd403b803b43bc
Amandine <amandine2.divay@gmail.com>
2020-02-26 12:43:08 +0100

    integration component Footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,11 +1,40 @@
 import React from 'react'; 
 import {withRouter} from 'react-router-dom'; 
+import {Route, Switch, Link, BrowserRouter as Router} from 'react-router-dom'; 
+import './../Footer.css'; 
+
+const style = {
+    listStyle: 'none',
+    textDecoration: 'none',
+    color:'#66242D',
+    fontSize:'1.5em',
+    textTransform:'uppercase',  
+}
 
 class Footer extends React.Component {
     render (){
         return (
-            <div>
-
+            <div className='footerDiv'>
+                <div className='discover'>
+                    <p>discover more project</p>
+                    <img src='img/Fleche.png'></img>
+                </div>
+                <footer>
+                    <ul className='linkProject'>
+                        <li>
+                        <Link to= '/Nomuse' style={style}>- nomuse -</Link>
+                        </li>
+                        <li>
+                        <Link to= '/Akuapeli' style={style}>- akuapéli -</Link>
+                        </li>
+                        <li>
+                        <Link to= '/Nomuse' style={style}>- misora -</Link>
+                        </li>
+                        <li>
+                        <Link to= '/Nomuse' style={style}>- puissance4 -</Link>
+                        </li>
+                    </ul>
+                </footer>
             </div>
         )
     }
